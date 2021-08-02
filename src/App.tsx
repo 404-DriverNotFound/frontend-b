e932047f62600c79cdecd6ca11b1bd28a60f1bcc
hyo-choi <hyochoi31@gmail.com>
2021-08-02 18:01:58 +0900

    Feat: API 서버와 연동하는 로직 추가
    - 초기 접속 시 세션이 유효하면 자동 로그인
    - 세션이 없을 때 로그인

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { useUserState } from './utils/hooks/useContext';
+import axios from 'axios';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { useDispatch, useUserState } from './utils/hooks/useContext';
 import LoginPage from './components/pages/LoginPage/LoginPage';
+import makeAPIPath from './utils/utils';
 
 const App = () => {
-  const { id } = useUserState();
+  /**
+   * FIXME: 아직 API 연동이 안 되어있어서 임시로 progress bar 넣어 둠
+   * 해결되면 지우거나 좀 더 괜찮은 모양새로 고칠 것
+   */
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const state = useUserState();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    // TODO: API 서버에서 쿠키에 담긴 세션 아이디 검증
+    setLoading(true);
+    /**
+     * FIXME: CORS 때문인지 서버가 안 떠있어서 그런 건지 콘솔에 오류가 뜸
+     * 문제 원인 파악해서 해결하고 FIXME 지워줄 것
+    */
+    axios.get(makeAPIPath('/users/me'))
+      .finally(() => { setLoading(false); })
+      .then((response) => {
+        const { id, name, avatar } = response.data;
+        dispatch({
+          type: 'login',
+          info: { id, name, avatar },
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          dispatch({ type: 'logout' });
+        } else {
+          // TODO: toast?
+        }
+      });
   }, []);
 
-  const children = id ? (
+  const children = state.id ? (
+    // FIXME: Main Page 컴포넌트가 없어 임시로 적어 둠
     <Switch>
-      <Route exact path="/" render={() => <h1>logged in</h1>} />
+      <Route exact path="/" render={() => <h1>main page</h1>} />
     </Switch>
   ) : (
     <Route path="/" component={LoginPage} />
   );
 
-  return children;
+  return isLoading ? <LinearProgress /> : children;
 };
 
 export default App;
