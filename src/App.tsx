420da98345d27315ee118ace16142544e6ecf607
hyo-choi <hyochoi31@gmail.com>
2021-08-02 16:53:46 +0900

    Feat: Context API 적용
    - React Context API, useReducer hook을 이용하여 App 전체 상태 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useUserState } from './utils/hooks/useContext';
 import LoginPage from './components/pages/LoginPage/LoginPage';
 
 const App = () => {
-  return (
+  const { id } = useUserState();
+
+  useEffect(() => {
+    // TODO: API 서버에서 쿠키에 담긴 세션 아이디 검증
+  }, []);
+
+  const children = id ? (
     <Switch>
-      <Route path="/" exact component={LoginPage} />
+      <Route exact path="/" render={() => <h1>logged in</h1>} />
     </Switch>
+  ) : (
+    <Route path="/" component={LoginPage} />
   );
+
+  return children;
 };
 
 export default App;
