a84e58665a06358ac1aa53bc24331b423920f552
hyo-choi <hyochoi31@gmail.com>
2021-08-05 12:27:49 +0900

    Feat: MainTemplate 완성
    Menu component를 추가하여 MainTemplate 제작 완료
    
    Resolves #28

--- src/components/templates/MainTemplate/MainTemplate.stories.tsx
+++ src/components/templates/MainTemplate/MainTemplate.stories.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 import { Meta } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import MainTemplate from './MainTemplate';
 
@@ -21,9 +22,11 @@ export const Default = () => {
   const classes = useStyles();
 
   return (
-    <MainTemplate
-      main={<div className={classes.div}>main</div>}
-      chat={<div className={classes.div}>chat</div>}
-    />
+    <BrowserRouter>
+      <MainTemplate
+        main={<div className={classes.div}>main. 배경색은 스토리에서 적용한 것입니다!</div>}
+        chat={<div className={classes.div}>chat. 배경색은 스토리에서 적용한 것입니다!</div>}
+      />
+    </BrowserRouter>
   );
 };
--- src/components/templates/MainTemplate/MainTemplate.tsx
+++ src/components/templates/MainTemplate/MainTemplate.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar } from '@material-ui/core';
+import Menu from '../../molecules/Menu/Menu';
 
 const useStyles = makeStyles({
   root: {
     margin: '0 auto',
+    marginTop: '64px',
     maxWidth: '1440px',
   },
   grid: {
     padding: '1em',
-    height: '90vh',
+    height: '85vh',
   },
 });
 
@@ -22,10 +23,9 @@ type MainTemplateProps = {
 const MainTemplate = ({ main, chat }: MainTemplateProps) => {
   const classes = useStyles();
 
-  // FIXME: Menu 컴포넌트 집어넣기
   return (
     <>
-      <AppBar>TEMP MENU</AppBar>
+      <Menu />
       <Grid container className={classes.root} justifyContent="space-evenly">
         <Grid item className={classes.grid} xs={8}>
           {main}
