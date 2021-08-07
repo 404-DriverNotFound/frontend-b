cdd2e0079a367aaeb6201109d21b949e9cbd52ac
hyo-choi <hyochoi31@gmail.com>
2021-08-07 10:59:32 +0900

    Feat: 2FA QR코드 정상 작동하도록 수정
    - URL.createObjectURL을 이용하여 QR코드 경로를 획득하도록 수정
    - 백엔드 API와 연동하여 정상 작동 확인

--- src/components/pages/MFARegisterPage/MFARegisterPage.tsx
+++ src/components/pages/MFARegisterPage/MFARegisterPage.tsx
@@ -1,5 +1,6 @@
 // import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import Button from '../../atoms/Button/Button';
 import LoginTemplate from '../../templates/LoginTemplate/LoginTemplate';
@@ -7,63 +8,31 @@ import { useAppDispatch } from '../../../utils/hooks/useContext';
 import makeAPIPath from '../../../utils/utils';
 
 const MFARegisterPage = () => {
-  const [QRCode, setQRCode] = useState<Blob | string>('');
   const [QRImageSrc, setQRSrc] = useState<string>('');
   const appDispatch = useAppDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     appDispatch({ type: 'loading' });
 
     fetch(makeAPIPath('/auth/otp/qrcode'), { credentials: 'include' })
-      // eslint-disable-next-line @typescript-eslint/no-use-before-define
-      .then(processChunkedResponse)
-      .then((result) => {
-        setQRCode(result);
-      })
+      .finally(() => { appDispatch({ type: 'endLoading' }); })
+      .then((response) => response.blob())
+      .then((blob) => setQRSrc(URL.createObjectURL(blob)))
       .catch((error) => {
         toast.error(error.message);
       });
-
-    function processChunkedResponse(response: any) {
-      let text = '';
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder();
-
-      function readChunk() {
-        // eslint-disable-next-line @typescript-eslint/no-use-before-define
-        return reader.read().then(appendChunks);
-      }
-
-      function appendChunks(result: any) {
-        const chunk = decoder.decode(result.value || new Uint8Array(), { stream: !result.done });
-        text += chunk;
-        if (result.done) {
-          return text;
-        }
-        return readChunk();
-      }
-      return readChunk();
-    }
   }, []);
 
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log(QRCode);
-    if (!QRCode) {
-      toast.error('QR코드를 받아오지 못했습니다.');
-      return;
-    }
-    const fileReader = new FileReader();
-    fileReader.onload = (event) => {
-      if (event.target) setQRSrc(String(event.target.result));
-    };
-    fileReader.readAsDataURL(QRCode as Blob);
-  }, [QRCode]);
+  const handleClick = () => {
+    // TODO: QR 정상 등록했는지 아닌지 확인해줘야 하지 않나?
+    history.push('/');
+  };
 
   return (
     <LoginTemplate
       input={<img alt="QR Code" src={QRImageSrc} />}
-      button={<Button>2FA 등록</Button>}
+      button={<Button onClick={handleClick}>2FA 등록</Button>}
     />
   );
 };
