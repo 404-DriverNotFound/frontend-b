ed2aacccbe768eb09384e568f6dc04027fa01b12
hyo-choi <hyochoi31@gmail.com>
2021-08-09 15:16:21 +0900

    Feat: Dialog component 제작
    Material UI의 Dialog 컴포넌트를 이용하여 molecule 컴포넌트 제작
    
    Resolves #41

---
+++ src/components/molecules/Dialog/Dialog.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import MaterialDialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+
+type DialogProps = {
+  title?: React.ReactNode,
+  content: React.ReactNode,
+  buttons?: React.ReactNode,
+  isOpen: boolean,
+  // eslint-disable-next-line no-unused-vars
+  handleClose: (event: {}, reason: 'backdropClick' | 'escapeKeyDown') => void,
+};
+
+const Dialog = ({
+  title, content, buttons, isOpen, handleClose,
+  // eslint-disable-next-line arrow-body-style
+}: DialogProps) => {
+  return (
+    <MaterialDialog
+      open={isOpen}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      {title && <DialogTitle>{title}</DialogTitle>}
+      <DialogContent>{content}</DialogContent>
+      {buttons && <DialogActions>{buttons}</DialogActions>}
+    </MaterialDialog>
+  );
+};
+
+Dialog.defaultProps = {
+  title: null,
+  buttons: null,
+};
+
+export default Dialog;
