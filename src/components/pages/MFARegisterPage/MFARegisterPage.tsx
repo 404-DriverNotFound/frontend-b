4f6f675c1131c67ffd20918a443c01ff0d286625
Jinwoo Kang <jikang@student.42seoul.kr>
2021-08-11 16:42:21 +0900

    Refactor: useDialog hook 작성
    하나의 Dialog로 재사용할 수 있는 custom hook 작성

--- src/components/pages/MFARegisterPage/MFARegisterPage.tsx
+++ src/components/pages/MFARegisterPage/MFARegisterPage.tsx
@@ -9,6 +9,7 @@ import LoginTemplate from '../../templates/LoginTemplate/LoginTemplate';
 import { useAppDispatch } from '../../../utils/hooks/useContext';
 import makeAPIPath from '../../../utils/utils';
 import Typo from '../../atoms/Typo/Typo';
+import useDialog from '../../../utils/hooks/useDialog';
 
 const useStyles = makeStyles({
   image: {
@@ -19,8 +20,9 @@ const useStyles = makeStyles({
 
 const MFARegisterPage = () => {
   const [QRImageSrc, setQRSrc] = useState<string>('');
-  const [isConfirmOpen, setConfirmOpen] = useState<boolean>(false);
-  const [isRegisterOpen, setRegisterOpen] = useState<boolean>(false);
+  const {
+    isOpen, setOpen, dialog, setDialog,
+  } = useDialog();
   const classes = useStyles();
   const appDispatch = useAppDispatch();
   const history = useHistory();
@@ -37,16 +39,16 @@ const MFARegisterPage = () => {
       });
   }, []);
 
-  const handleClick = () => {
-    setConfirmOpen(true);
-  };
-
   const buttons = (
     <>
-      <Button variant="text" onClick={() => { setConfirmOpen(false); }}>아니오, 아직 등록하지 않았습니다</Button>
+      <Button variant="text" onClick={() => { setOpen(false); }}>아니오, 아직 등록하지 않았습니다</Button>
       <Button onClick={() => {
-        setConfirmOpen(false);
-        setRegisterOpen(true);
+        setDialog({
+          title: '회원가입 완료',
+          content: '회원가입이 완료되어 로그인 화면으로 돌아갑니다. 서비스를 이용하시려면 로그인 해주세요.',
+          buttons: <Button variant="text" onClick={() => { history.push('/'); }}>확인</Button>,
+          onClose: () => { history.push('/'); },
+        });
       }}
       >
         네, 등록을 완료했습니다
@@ -54,21 +56,24 @@ const MFARegisterPage = () => {
     </>
   );
 
+  const handleClick = () => {
+    setDialog({
+      title: 'QR 코드 등록 확인',
+      content: 'Google OTP 앱에 QR 코드를 정상적으로 등록하셨나요? QR 코드를 등록하지 않고 해당 페이지를 벗어나면 2FA 인증이 불가능합니다. 확인하셨다면 아래 버튼을 클릭해주세요.',
+      buttons,
+      onClose: () => { setOpen(false); },
+    });
+    setOpen(true);
+  };
+
   return (
     <>
       <Dialog
-        isOpen={isConfirmOpen}
-        title="QR 코드 등록 확인"
-        content="Google OTP 앱에 QR 코드를 정상적으로 등록하셨나요? QR 코드를 등록하지 않고 해당 페이지를 벗어나면 2FA 인증이 불가능합니다. 확인하셨다면 아래 버튼을 클릭해주세요."
-        buttons={buttons}
-        onClose={() => { setConfirmOpen(false); }}
-      />
-      <Dialog
-        isOpen={isRegisterOpen}
-        title="회원가입 완료"
-        content="회원가입이 완료되어 로그인 화면으로 돌아갑니다. 서비스를 이용하시려면 로그인 해주세요."
-        buttons={<Button variant="text" onClick={() => { history.push('/'); }}>확인</Button>}
-        onClose={() => { history.push('/'); }}
+        isOpen={isOpen}
+        title={dialog.title}
+        content={dialog.content}
+        buttons={dialog.buttons}
+        onClose={dialog.onClose}
       />
       <LoginTemplate
         input={(
