86b95b67d75f7086ffb2dceaf5cf2c453bff07b6
Jinwoo Kang <jikang@student.42seoul.kr>
2021-08-18 20:56:55 +0900

    Fix: Input Focusing bug, Input console error
    https://github.com/404-DriverNotFound/frontend-b/issues/63#issuecomment-901032521

--- src/components/organisms/ChannelInfoForm/ChannelInfoForm.tsx
+++ src/components/organisms/ChannelInfoForm/ChannelInfoForm.tsx
@@ -7,7 +7,7 @@ import Typo from '../../atoms/Typo/Typo';
 const ChannelInfoForm = () => {
   const [channelName, setChannelName] = useState<string>('');
   const [isValidChannelName, setValidChannelName] = useState<boolean>(false);
-  const [helperTextChannelName, setHelperTextChannelName] = useState<string>('영문+숫자 5-30자');
+  const [helperTextChannelName, setHelperTextChannelName] = useState<string>('영문+숫자 5-25자');
 
   const [isToggleChecked, setToggleCheck] = useState(false);
 
@@ -22,25 +22,25 @@ const ChannelInfoForm = () => {
   const handleChannelNameChange = (event: React.ChangeEvent<Element>) => {
     const { value } = (event as React.ChangeEvent<HTMLInputElement>).target;
     if (value.length > 25) return;
-    if (value.length > 4 && /^[A-Za-z0-9]{5,12}$/.test(value)) {
+    if (/^[A-Za-z0-9]{5,25}$/.test(value)) {
       setValidChannelName(true);
       setHelperTextChannelName('사용할 수 있는 채널이름');
     } else {
       setValidChannelName(false);
-      setHelperTextChannelName('영문+숫자 5-30자');
+      setHelperTextChannelName('영문+숫자 5-25자');
     }
     setChannelName(value);
   };
 
   const handlePasswordChange = (event: React.ChangeEvent<Element>) => {
     const { value } = (event as React.ChangeEvent<HTMLInputElement>).target;
+
     if (value.length > 12) return;
     if (/^[A-Za-z0-9]{4,12}$/.test(value)) {
       setValidPassword(true);
       setHelperTextPassword('사용할 수 있는 비밀번호');
     } else setValidPassword(false);
-    setPassword(value);
-    // FIXME: 비밀번호 검증 후, 비밀번호를 바꿨을 때 생기는 문제를 위한 코드
+    // NOTE: 아래는 비밀번호 검증 후, 비밀번호를 바꿨을 때 생기는 문제를 위한 코드
     if (checkPassword === value) {
       setValidCheckPassword(true);
       setHelperTextCheckPassword('비밀번호 일치');
@@ -48,10 +48,12 @@ const ChannelInfoForm = () => {
       setValidCheckPassword(false);
       setHelperTextCheckPassword('비밀번호 일치하지 않음');
     }
+    setPassword(value);
   };
 
   const handleCheckPasswordChange = (event: React.ChangeEvent<Element>) => {
     const { value } = (event as React.ChangeEvent<HTMLInputElement>).target;
+
     if (value.length > 12) return;
     if (value === password) {
       setValidCheckPassword(true);
@@ -63,53 +65,51 @@ const ChannelInfoForm = () => {
     setCheckPassword(value);
   };
 
-  const PasswordInput = () => (
-    <Grid container flex-direction="column">
-      <Grid item container>
-        <Input
-          onChange={handlePasswordChange}
-          label="비밀번호 입력 *"
-          type="password"
-          value={password}
-          helperText={helperTextPassword}
-          error={!isValidPassword}
-        />
-      </Grid>
-      <Grid item container>
-        <Input
-          onChange={handleCheckPasswordChange}
-          label="비밀번호 검증 입력 *"
-          type="password"
-          value={checkPassword}
-          helperText={helperTextCheckPassword}
-          error={!isValidCheckPassword}
-        />
-      </Grid>
-    </Grid>
-  );
-
   return (
     <Grid container flex-direction="column" alignItems="center">
-      <Grid item>
-        <Input
-          onChange={handleChannelNameChange}
-          label="채널명 입력 *"
-          value={channelName}
-          helperText={helperTextChannelName}
-          error={!isValidChannelName}
-        />
-      </Grid>
-      <Grid item container>
-        <Switch
-          name="private toggle"
-          checked={isToggleChecked}
-          onChange={() => { setToggleCheck(!isToggleChecked); }}
-        />
-        <Typo>Private</Typo>
-      </Grid>
-      <Grid item>
-        {isToggleChecked && <PasswordInput />}
-      </Grid>
+      <form>
+        <Grid item>
+          <Input
+            onChange={handleChannelNameChange}
+            label="채널명 입력 *"
+            value={channelName}
+            helperText={helperTextChannelName}
+            error={!isValidChannelName}
+          />
+        </Grid>
+        <Grid item container>
+          <Switch
+            name="private toggle"
+            checked={isToggleChecked}
+            onChange={() => { setToggleCheck(!isToggleChecked); }}
+          />
+          <Typo>Private</Typo>
+        </Grid>
+        <Grid item>
+          {isToggleChecked && (
+          <>
+            <Input
+              onChange={handlePasswordChange}
+              label="비밀번호 입력 *"
+              type="password"
+              value={password}
+              helperText={helperTextPassword}
+              error={!isValidPassword}
+              autoComplete
+            />
+            <Input
+              onChange={handleCheckPasswordChange}
+              label="비밀번호 검증 입력 *"
+              type="password"
+              value={checkPassword}
+              helperText={helperTextCheckPassword}
+              error={!isValidCheckPassword}
+              autoComplete
+            />
+          </>
+          )}
+        </Grid>
+      </form>
     </Grid>
   );
 };
