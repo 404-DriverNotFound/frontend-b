19c7bf6b81fcd1b35f1197266eae6f1f15803c5b
hyo-choi <hyochoi31@gmail.com>
2021-09-03 15:57:54 +0900

    Feat: Chat Story 수정
    - 각 ListItem에 고유의 키(메시지 전송 시간) 등록
    - MainTemplate의 main props를 임의의 컴포넌트로 변경
      - CommunityPage에는 Routing 로직이 포함되어 있어 채팅 테스트용으로는
        부적절하다 판단하였습니다.

--- src/components/Chat.stories.tsx
+++ src/components/Chat.stories.tsx
@@ -6,21 +6,26 @@ import ListItem from './atoms/ListItem/ListItem';
 import List from './atoms/List/List';
 import ChatInput from './atoms/ChatInput/ChatInput';
 import { ContextProvider } from '../utils/hooks/useContext';
-import CommunityPage from './pages/CommunityPage/CommunityPage';
+import Typo from './atoms/Typo/Typo';
 
 export default {
   component: MainTemplate,
   title: 'tests/Chat',
 } as Meta;
 
-const addNewChat = (prev: string[], newChat: string) => {
+type ChatType = {
+  content: string,
+  date: string,
+};
+
+const addNewChat = (prev: ChatType[], content: string) => {
   const temp = prev.slice();
-  temp.unshift(newChat);
+  temp.unshift({ content, date: Date.now().toString() });
   return temp;
 };
 
 const Chat = () => {
-  const [chat, setChat] = useState<string[]>(['0']);
+  const [chat, setChat] = useState<ChatType[]>([]);
   const [value, setValue] = useState<string>('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -30,7 +35,7 @@ const Chat = () => {
   return (
     <>
       <List height="75vh" scroll reverse>
-        {chat.map((one) => <ListItem>{one}</ListItem>)}
+        {chat.map(({ content, date }) => <ListItem key={date}>{content}</ListItem>)}
       </List>
       <ChatInput
         onSubmit={(e) => {
@@ -50,7 +55,15 @@ export const ChatTest = () => (
   <BrowserRouter>
     <ContextProvider>
       <MainTemplate
-        main={<CommunityPage />}
+        main={(
+          <>
+            <Typo variant="h3" gutterBottom>Chat Test Page</Typo>
+            <Typo variant="h5">List</Typo>
+            <List />
+            <Typo variant="h5">List</Typo>
+            <List height="35vh" />
+          </>
+        )}
         chat={<Chat />}
       />
     </ContextProvider>
