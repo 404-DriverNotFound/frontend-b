3c9bb1b992cf90a06862f9c5a1e20ec15cad05b7
hyo-choi <hyochoi31@gmail.com>
2021-09-03 15:56:43 +0900

    Refactor: ChatInput의 scrollbar 관련 style 삭제
    ChatInput에 multiline 속성을 넣지 않기로 결정했으므로
    scrollbar 관련 style을 삭제

--- src/components/atoms/ChatInput/ChatInput.tsx
+++ src/components/atoms/ChatInput/ChatInput.tsx
@@ -1,23 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-
-const useStyles = makeStyles({
-  root: {
-    '&::-webkit-scrollbar': {
-      width: '10px',
-    },
-    '&::-webkit-scrollbar-thumb': {
-      backgroundColor: 'lightgray',
-      borderRadius: '5px',
-    },
-    '&::-webkit-scrollbar-track': {
-      backgroundColor: 'transparent',
-      overflow: 'hidden',
-    },
-    scrollbarColor: 'lightgray transparent',
-  },
-});
 
 type ChatInputProps = {
   onSubmit: React.FormEventHandler<HTMLFormElement>,
@@ -25,22 +7,17 @@ type ChatInputProps = {
   value?: string,
 };
 
-const ChatInput = ({ onSubmit, onChange, value }: ChatInputProps) => {
-  const classes = useStyles();
-
-  return (
-    <form onSubmit={onSubmit}>
-      <TextField
-        className={classes.root}
-        onChange={onChange}
-        value={value}
-        label="Chat on here!"
-        variant="outlined"
-        fullWidth
-      />
-    </form>
-  );
-};
+const ChatInput = ({ onSubmit, onChange, value }: ChatInputProps) => (
+  <form onSubmit={onSubmit}>
+    <TextField
+      onChange={onChange}
+      value={value}
+      label="Chat on here!"
+      variant="outlined"
+      fullWidth
+    />
+  </form>
+);
 
 ChatInput.defaultProps = {
   value: '',
