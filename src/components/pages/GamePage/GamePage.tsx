4c0c840abb3f1e58f9ae103d25a1c696184170ee
Jinwoo Kang <jikang@student.42seoul.kr>
2021-09-15 19:26:29 +0900

    Feat: App에 연결 각 4개 game page 임시 라우팅
    - game option에 따른 임시 game page 라우팅

--- src/components/pages/GamePage/GamePage.tsx
+++ src/components/pages/GamePage/GamePage.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  Route, Switch, useHistory, Redirect,
+} from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import GameOptionCard from '../../molecules/GameOptionCard/GameOptionCard';
+
+const MAIN_GAME_PAGE = '/game';
+const CLASSIC_PLAY_PATH = '/game/playclassic';
+const SPEED_PLAY_PATH = '/game/playspeed';
+const REVERSE_PLAY_PATH = '/game/playreverse';
+const WATCH_PLAY_PATH = '/game/watch';
 
 const useStyles = makeStyles({
   root: {
@@ -13,25 +22,41 @@ const useStyles = makeStyles({
   },
 });
 
-const GamePage = () => {
+const GameMainPage = () => {
+  const history = useHistory();
   const classes = useStyles();
 
   return (
     <Grid className={classes.root} container justifyContent="center" alignItems="center" spacing={1}>
       <Grid item xs={6}>
-        <GameOptionCard option="classic" onClick={() => {}} />
+        <GameOptionCard option="classic" onClick={() => { history.push(CLASSIC_PLAY_PATH); }} />
       </Grid>
       <Grid item xs={6}>
-        <GameOptionCard option="speed" onClick={() => {}} />
+        <GameOptionCard option="speed" onClick={() => { history.push(SPEED_PLAY_PATH); }} />
       </Grid>
       <Grid item xs={6}>
-        <GameOptionCard option="reverse" onClick={() => {}} />
+        <GameOptionCard option="reverse" onClick={() => { history.push(REVERSE_PLAY_PATH); }} />
       </Grid>
       <Grid item xs={6}>
-        <GameOptionCard option="watch" onClick={() => {}} />
+        <GameOptionCard option="watch" onClick={() => { history.push(WATCH_PLAY_PATH); }} />
       </Grid>
     </Grid>
   );
 };
+
+const GamePage = () => (
+  <>
+    <Switch>
+      <Route exact path={MAIN_GAME_PAGE} component={GameMainPage} />
+      <Route exact path={CLASSIC_PLAY_PATH} render={() => <h1>Classic play</h1>} />
+      <Route exact path={SPEED_PLAY_PATH} render={() => <h1>Speedy play</h1>} />
+      <Route exact path={REVERSE_PLAY_PATH} render={() => <h1>Reverse play</h1>} />
+      <Route exact path={WATCH_PLAY_PATH} render={() => <h1>Watch play</h1>} />
+      <Route path="/">
+        <Redirect to={MAIN_GAME_PAGE} />
+      </Route>
+    </Switch>
+  </>
+);
 
 export default GamePage;
