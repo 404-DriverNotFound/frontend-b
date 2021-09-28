d34bb7e314b8888fe98630af386f4b6f3e0df6c3
hyo-choi <hyochoi31@gmail.com>
2021-09-28 20:09:33 +0900

    Refactor: GameOptionCard가 GameModeType을 이용하록 수정

--- src/components/pages/GamePage/GamePage.tsx
+++ src/components/pages/GamePage/GamePage.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Route, Switch, useHistory, Redirect,
 } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import GameOptionCard from '../../molecules/GameOptionCard/GameOptionCard';
 import GameWatchPage from '../GameWatchPage/GameWatchPage';
+import { useAppState } from '../../../utils/hooks/useAppContext';
+import useDialog from '../../../utils/hooks/useDialog';
+import Button from '../../atoms/Button/Button';
+import Dialog from '../../molecules/Dialog/Dialog';
+import { GameModeType } from '../../../types/Match';
+import Typo from '../../atoms/Typo/Typo';
 
 const MAIN_GAME_PAGE = '/game';
 const CLASSIC_PLAY_PATH = '/game/playclassic';
@@ -21,43 +28,119 @@ const useStyles = makeStyles({
     borderRadius: '10px',
     padding: '5px',
   },
+  progress: {
+    margin: '1em',
+  },
 });
 
 const GameMainPage = () => {
   const history = useHistory();
   const classes = useStyles();
+  const { socket } = useAppState();
+  const [mode, setMode] = useState<GameModeType | null>(null);
+  const {
+    isOpen, setOpen, dialog, setDialog,
+  } = useDialog();
+
+  const handleReady = (position: 'LEFT' | 'RIGHT', setting: any) => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log(position, setting);
+      socket?.off('ready');
+      setOpen(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    if (!mode) {
+      socket?.off('ready');
+      setOpen(false);
+    } else if (mode === 'CLASSIC') {
+      setDialog({ ...dialog, title: 'Classic Game Matching...' });
+      socket?.on('ready', handleReady);
+      setOpen(true);
+      socket?.emit('waiting');
+    } // FIXME: 다른 모드도 추가하고 공통부분 밖으로 빼기
+    // TODO emit할 때 roomId 같이 보내달라고 요청하기
+  }, [mode]);
+
+  useEffect(() => {
+    setDialog({
+      title: 'default',
+      content: (
+        <Grid container direction="column" justifyContent="center" alignItems="center">
+          <CircularProgress size={100} className={classes.progress} aria-busy />
+          <Typo variant="subtitle1" align="center">매칭 가능한 유저를 찾는 중입니다</Typo>
+        </Grid>
+      ),
+      buttons: <Button onClick={() => setMode(null)}>매칭 취소</Button>,
+      onClose: () => setMode(null),
+    });
+  }, []);
 
   return (
     <Grid className={classes.root} container justifyContent="space-evenly" alignItems="center" spacing={3}>
+      <Dialog
+        title={dialog.title}
+        content={dialog.content}
+        buttons={dialog.buttons}
+        isOpen={isOpen}
+        onClose={dialog.onClose}
+      />
       <Grid item xs={6}>
-        <GameOptionCard option="classic" onClick={() => { history.push(CLASSIC_PLAY_PATH); }} />
+        <GameOptionCard option="CLASSIC" onClick={() => setMode('CLASSIC')} />
       </Grid>
       <Grid item xs={6}>
-        <GameOptionCard option="speed" onClick={() => { history.push(SPEED_PLAY_PATH); }} />
+        <GameOptionCard option="SPEED" onClick={() => setMode('SPEED')} />
       </Grid>
       <Grid item xs={6}>
-        <GameOptionCard option="reverse" onClick={() => { history.push(REVERSE_PLAY_PATH); }} />
+        <GameOptionCard option="REVERSE" onClick={() => setMode('REVERSE')} />
       </Grid>
       <Grid item xs={6}>
-        <GameOptionCard option="watch" onClick={() => { history.push(WATCH_PLAY_PATH); }} />
+        <GameOptionCard option="WATCH" onClick={() => { history.push(WATCH_PLAY_PATH); }} />
       </Grid>
     </Grid>
   );
 };
 
-const GamePage = () => (
-  <>
-    <Switch>
-      <Route exact path={MAIN_GAME_PAGE} component={GameMainPage} />
-      <Route exact path={CLASSIC_PLAY_PATH} render={() => <h1>Classic play</h1>} />
-      <Route exact path={SPEED_PLAY_PATH} render={() => <h1>Speedy play</h1>} />
-      <Route exact path={REVERSE_PLAY_PATH} render={() => <h1>Reverse play</h1>} />
-      <Route path={WATCH_PLAY_PATH} component={GameWatchPage} />
-      <Route exact path="/">
-        <Redirect to={MAIN_GAME_PAGE} />
-      </Route>
-    </Switch>
-  </>
-);
+const GamePage = () => {
+  const { socket } = useAppState();
+  return (
+    <>
+      {/* FIXME: button들 삭제해야 함 */}
+      <Button onClick={() => {
+        // eslint-disable-next-line no-console
+        if (socket?.emit('waiting')) console.log('emit waiting');
+      }}
+      >
+        waiting
+      </Button>
+      <Button onClick={() => {
+        // eslint-disable-next-line no-console
+        if (socket?.emit('ready')) console.log('emit ready');
+      }}
+      >
+        ready
+      </Button>
+      <Button onClick={() => {
+        // eslint-disable-next-line no-console
+        if (socket?.emit('leaveGame')) console.log('emit leave game');
+      }}
+      >
+        leaveGame
+      </Button>
+      <Switch>
+        <Route exact path={MAIN_GAME_PAGE} component={GameMainPage} />
+        <Route exact path={CLASSIC_PLAY_PATH} render={() => <h1>Classic play</h1>} />
+        <Route exact path={SPEED_PLAY_PATH} render={() => <h1>Speedy play</h1>} />
+        <Route exact path={REVERSE_PLAY_PATH} render={() => <h1>Reverse play</h1>} />
+        <Route path={WATCH_PLAY_PATH} component={GameWatchPage} />
+        <Route exact path="/">
+          <Redirect to={MAIN_GAME_PAGE} />
+        </Route>
+      </Switch>
+    </>
+  );
+};
 
 export default GamePage;
