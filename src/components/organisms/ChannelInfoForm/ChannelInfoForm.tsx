b30e8799f7e22829efb64178ed86bb24b5f33d03
hyo-choi <hyochoi31@gmail.com>
2021-12-23 14:28:04 +0900

    Refactor: 채널 이름 규칙 변경(자유도) (#119)
    * Refactor: 채널 이름 규칙 변경
    
    - 채널 이름 자유도가 낮아 모음, 자음 및 특수문자 허용

--- src/components/organisms/ChannelInfoForm/ChannelInfoForm.tsx
+++ src/components/organisms/ChannelInfoForm/ChannelInfoForm.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles({
 });
 
 const CHANNEL_NAME_AVAILABLE = '채널명 중복검사를 실행하세요';
-const CHANNEL_NAME_HELPER_TEXT = '앞뒤 공백 없는 한글/영문/숫자/공백 3-18자';
+const CHANNEL_NAME_HELPER_TEXT = '앞뒤 공백 없는 3-18자(\\,% 제외)';
 const CHANNEL_PASSWORD_AVAILABLE = '사용할 수 있는 비밀번호';
 const CHANNEL_PASSWORD_HELPER_TEXT = '공백 제외 모든 문자+숫자 4-32자';
 const PASSWORD_CHECK_YES = '비밀번호 일치';
@@ -59,7 +59,7 @@ const ChannelInfoForm = ({ setOpen, channel }: ChannelInfoFormProps) => {
   const handleChannelNameChange = (event: React.ChangeEvent<Element>) => {
     const { value } = (event as React.ChangeEvent<HTMLInputElement>).target;
     if (value.length > 18) return;
-    if (/^[^\s]+(\s+[^\s]+)*$/.test(value) && /^[0-9a-zA-z\uAC00-\uD7A3\s]{3,18}$/.test(value)) {
+    if (/^[^\s]+(\s+[^\s]+)*$/.test(value) && (/^[^\\%]{3,18}$/).test(value)) {
       setValidChannelName(true);
       setHelperTextChannelName(CHANNEL_NAME_AVAILABLE);
     } else {
@@ -186,7 +186,7 @@ const ChannelInfoForm = ({ setOpen, channel }: ChannelInfoFormProps) => {
     if (channel) {
       setValidChannelName(true);
       setDuplicateChecked(true);
-      setChannelName(channel);
+      setChannelName(decodeURIComponent(channel));
       asyncGetRequest(makeAPIPath(`/channels/${channel}`))
         .then(({ data }) => {
           setToggleCheck(data.password !== null);
